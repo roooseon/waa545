387463c059b27d740a53c23c95d59d6c26a5b8fb
Ramesh KC <kcramesh8@gmail.com>
2017-01-18 17:02:02 -0600

    Http Session implementation instead of SessionAttribute.

--- w1d3/w1d3lab1/src/main/java/com/waa/w1d3lab1/controller/LoginController.java
+++ w1d3/w1d3lab1/src/main/java/com/waa/w1d3lab1/controller/LoginController.java
@@ -1,18 +1,17 @@
 package com.waa.w1d3lab1.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 import com.waa.w1d3lab1.domain.User;
 import com.waa.w1d3lab1.repository.UserService;
 
 @Controller
-@SessionAttributes("user")
 public class LoginController {
 
 	@Autowired
@@ -24,10 +23,10 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String processLoginForm(User user, Model model) {
+	public String processLoginForm(User user, Model model, HttpSession session) {
 
 		if (userService.authenticat(user)) {
-			model.addAttribute("user", user);
+			session.setAttribute("user", user);
 			return "redirect:/welcome";
 
 		} else {
@@ -42,8 +41,8 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value = "/logout")
-	public String logout(SessionStatus status) {
-		status.setComplete();
+	public String logout(HttpSession session) {
+		session.invalidate();;
 		
 		return "login";
 	}
