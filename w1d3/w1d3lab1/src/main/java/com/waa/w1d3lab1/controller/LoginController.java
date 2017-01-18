bf8f652e0ae2d4cb36022c688f8990e2d45bb22c
Ramesh KC <kcramesh8@gmail.com>
2017-01-18 16:12:16 -0600

    Login Controller without implementation of state maintenance.

--- w1d3/w1d3lab1/src/main/java/com/waa/w1d3lab1/controller/LoginController.java
+++ w1d3/w1d3lab1/src/main/java/com/waa/w1d3lab1/controller/LoginController.java
@@ -1,19 +1,40 @@
 package com.waa.w1d3lab1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.waa.w1d3lab1.domain.User;
+import com.waa.w1d3lab1.repository.UserService;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/", method= RequestMethod.GET)
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String getLoginForm() {
 		return "login";
 	}
 	
-	@RequestMapping(value="/login", method = RequestMethod.POST)
-	public String processLoginForm() {
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String processLoginForm(User user, Model model) {
+		
+		if (userService.authenticat(user)) {
+			model.addAttribute("user", user);
+			return "welcome";
+		
+		} else {
+			model.addAttribute("errorMessage", "username and/or password is invalid");
+			return "login";
+		}
+	}
+	
+	@RequestMapping(value = "/welcome", method =  RequestMethod.GET) 
+	public String getWelcomePage() {
 		return "welcome";
 	}
 }
