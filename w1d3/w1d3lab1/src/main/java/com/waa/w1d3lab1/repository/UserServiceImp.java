7ed0464b466e5529a05d8453f0b2e4bf8f8eea9b
Ramesh KC <kcramesh8@gmail.com>
2017-01-18 15:21:15 -0600

    User Service Inteface authernication method implementation.

---
+++ w1d3/w1d3lab1/src/main/java/com/waa/w1d3lab1/repository/UserServiceImp.java
@@ -0,0 +1,20 @@
+package com.waa.w1d3lab1.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.waa.w1d3lab1.domain.User;
+
+@Repository
+public class UserServiceImp implements UserService {
+
+	@Override
+	public boolean authenticat(User user) {
+		if ("ramesh".equals(user.getUsername()) && "test123".equals(user.getPassword())) {
+			return true;
+		
+		} else {
+			return false;
+		}
+	}
+
+}
