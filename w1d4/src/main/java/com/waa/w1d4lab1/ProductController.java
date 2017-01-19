c5dae77b13c5513e773c48ba42b4df6108d95b03
Ramesh KC <kcramesh8@gmail.com>
2017-01-19 13:55:15 -0600

    w1d4lab1 project setup.

---
+++ w1d4/src/main/java/com/waa/w1d4lab1/ProductController.java
@@ -0,0 +1,14 @@
+package com.waa.w1d4lab1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+	
+	@RequestMapping(value = "/",  method = RequestMethod.GET)
+	public String getWelcomePage() {
+		return "welcome";
+	}
+}
