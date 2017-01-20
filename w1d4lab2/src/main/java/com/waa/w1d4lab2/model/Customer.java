4c06ecbc27cdc8552ae1c921ed216b662e88b414
Ramesh KC <kcramesh8@gmail.com>
2017-01-20 14:18:00 -0600

    Customer domain add.

---
+++ w1d4lab2/src/main/java/com/waa/w1d4lab2/model/Customer.java
@@ -0,0 +1,46 @@
+package com.waa.w1d4lab2.model;
+
+import java.util.Date;
+
+public class Customer {
+	
+	public String username;
+	public String address;
+	public Date dateOfBirth;
+	public String password;
+	public String sex;
+	
+	public Customer() {}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+}
