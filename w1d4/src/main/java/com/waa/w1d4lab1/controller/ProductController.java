adb2a282968d0e4b9c3314f21137c097f65b7f56
Ramesh KC <kcramesh8@gmail.com>
2017-01-19 21:36:26 -0600

    product edit and delete function implementation.

--- w1d4/src/main/java/com/waa/w1d4lab1/controller/ProductController.java
+++ w1d4/src/main/java/com/waa/w1d4lab1/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -52,7 +53,49 @@ public class ProductController {
 	public String getAllProducts(Model model) {
 		model.addAttribute("products", productRepository.getAllProducts());
 		
-		
 		return "products";
+	}
+	
+	@RequestMapping(value = "/product/{productId}", method = RequestMethod.GET)
+	public String getProductById(@PathVariable String productId, Model model) {
+		model.addAttribute("product", productRepository.getProductById(productId));
+		
+		return "edit-product";
+	}
+	
+	@RequestMapping(value = "/product/{productId}", method = RequestMethod.POST)
+	public String editProduct(@ModelAttribute Product product, @PathVariable("productId") String productId, Model model, 
+			BindingResult result){
+		
+		String[] suppressedFields = result.getSuppressedFields();
+		
+		if (suppressedFields.length > 0) {
+			throw new RuntimeException("Attempting to bind disallowed fields : " + 
+								StringUtils.arrayToCommaDelimitedString(suppressedFields));
+		}
+		
+		boolean checkEdit = productRepository.editProductById(productId, product);
+		if (checkEdit) {
+			return "redirect:/products";
+		
+		} else {
+			model.addAttribute("product", productRepository.getProductById(productId));
+			return "edit-product";
+		}
+		
+	}
+	
+	@RequestMapping(value = "/product/delete/{productId}",  method = RequestMethod.GET)
+	public String deleteProduct(@PathVariable String productId){
+		
+		boolean deleteStatus  = productRepository.deleteProductById(productId);
+		if (deleteStatus) {
+			return "redirect:/products";
+		
+		}
+		else {
+			return "redirect:/products";
+		}
+		
 	}
 }
