bec2653c0af7a3b4c668f3541551540407ff5646
Ramesh KC <kcramesh8@gmail.com>
2017-01-19 21:40:33 -0600

    service folder added but do not write anything.

---
+++ w1d4/src/main/java/com/waa/w1d4lab1/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.waa.w1d4lab1.service;
+
+public interface ProductService {
+}
---
+++ w1d4/src/main/java/com/waa/w1d4lab1/service/ProductServiceImp.java
@@ -0,0 +1,9 @@
+package com.waa.w1d4lab1.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImp implements ProductService {
+	
+}
