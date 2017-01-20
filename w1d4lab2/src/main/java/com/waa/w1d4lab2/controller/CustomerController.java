bb14fbb7b472247a88044d3500df4a28d052edb7
Ramesh KC <kcramesh8@gmail.com>
2017-01-20 15:26:55 -0600

    Adding customer Controller for.

--- w1d4lab2/src/main/java/com/waa/w1d4lab2/controller/CustomerController.java
+++ w1d4lab2/src/main/java/com/waa/w1d4lab2/controller/CustomerController.java
@@ -1,19 +1,22 @@
-package com.waa.w1d4lab2;
+package com.waa.w1d4lab2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.waa.w1d4lab2.model.Customer;
 
 @Controller
 public class CustomerController {
 	
 	@RequestMapping(value = "/customer", method = RequestMethod.GET)
-	public String getCustomerLogin() {
-		return "customer";
+	public String getCustomerLogin(@ModelAttribute("newCustomer") Customer newCustomer) {
+		return "add-customer";
 	}
 	
 	@RequestMapping(value = "/customer", method = RequestMethod.POST)
-	public String processCustomerLoginForm() {
-		return "customers";
+	public String processCustomerLoginForm(@ModelAttribute("newCustomer") Customer newCustomer) {
+		return "show-customers";
 	}
 }
