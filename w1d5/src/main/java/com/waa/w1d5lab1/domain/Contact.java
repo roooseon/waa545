8159b0554258ade3f468b6f59a6cf3e453682302
Ramesh KC <kcramesh8@gmail.com>
2017-01-20 17:50:43 -0600

    contact domain class add.

---
+++ w1d5/src/main/java/com/waa/w1d5lab1/domain/Contact.java
@@ -0,0 +1,25 @@
+package com.waa.w1d5lab1.domain;
+
+
+public class Contact {
+	
+	public String firstname;
+	public String lastname;
+	
+	public Contact() {}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+}
