589371ba29790e211d6246d08a55652ebe54a869
Ramesh KC <kcramesh8@gmail.com>
2017-01-22 16:55:26 -0600

    Customer domain with 2 fields value customer and javaSkills.

---
+++ exam/src/main/java/com/waa/exam/domain/Customer.java
@@ -0,0 +1,24 @@
+package com.waa.exam.domain;
+
+public class Customer {
+	
+	private String country;
+	private String javaSkills;
+	
+	public Customer() {}
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getJavaSkills() {
+		return javaSkills;
+	}
+	public void setJavaSkills(String javaSkills) {
+		this.javaSkills = javaSkills;
+	}
+	
+	
+}
