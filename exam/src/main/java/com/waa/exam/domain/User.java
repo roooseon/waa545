cae1983dd847dbf336f449bb078edbb7f33fc941
Ramesh KC <kcramesh8@gmail.com>
2017-01-22 20:21:54 -0600

    add User domain class.

---
+++ exam/src/main/java/com/waa/exam/domain/User.java
@@ -0,0 +1,35 @@
+package com.waa.exam.domain;
+
+public class User {
+	private String username;
+	private String password;
+	private String address;
+	
+	public User() {}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
