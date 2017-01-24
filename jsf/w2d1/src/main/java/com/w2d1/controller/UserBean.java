1af10e2205e17750ac0a718978425a0aaaa93238
Ramesh KC <kcramesh8@gmail.com>
2017-01-24 16:21:14 -0600

    jsf project setup.

---
+++ jsf/w2d1/src/main/java/com/w2d1/controller/UserBean.java
@@ -0,0 +1,34 @@
+package com.w2d1.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("user")
+@SessionScoped
+public class UserBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String authenticate() {
+		return "welcome";
+	}
+	
+}
