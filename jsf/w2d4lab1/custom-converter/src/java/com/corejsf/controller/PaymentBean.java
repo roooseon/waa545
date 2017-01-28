b811b446d7b636a678edc0306ac3fe4b97b18c28
Ramesh KC <kcramesh8@gmail.com>
2017-01-27 19:52:16 -0600

    custom-converter project setup.

---
+++ jsf/w2d4lab1/custom-converter/src/java/com/corejsf/controller/PaymentBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.controller;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PaymentBean implements Serializable {
+    private String amount;
+    private String card = "";
+    private Date date = new Date();
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
