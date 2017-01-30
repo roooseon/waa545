4653a4d6a7533ffafc5194a2bf7ce99a4a588393
Ramesh KC <kcramesh8@gmail.com>
2017-01-30 16:43:03 -0600

    implementation of event listener tag CountryListener.

---
+++ jsf/valuechange/src/java/com/jsfcore/controller/CountryListener.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsfcore.controller;
+
+import java.util.Locale;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ValueChangeListener;
+
+/**
+ *
+ * @author ramesh
+ */
+public class CountryListener implements ValueChangeListener {
+
+    @Override
+    public void processValueChange(ValueChangeEvent event) throws AbortProcessingException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        if ("US".equals(event.getNewValue())) {
+            context.getViewRoot().setLocale(Locale.US);
+        
+        } else {
+            context.getViewRoot().setLocale(Locale.CANADA);
+        } 
+    }
+}
