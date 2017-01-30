dad45d86ded30328b27ec9d7b590c622b2dfa861
Ramesh KC <kcramesh8@gmail.com>
2017-01-30 11:59:51 -0600

    add properties for registerform.

--- jsf/valuechange/src/java/com/jsfcore/controller/RegisterForm.java
+++ jsf/valuechange/src/java/com/jsfcore/controller/RegisterForm.java
@@ -6,11 +6,53 @@
 package com.jsfcore.controller;
 
 import java.io.Serializable;
+import java.util.Locale;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
-/**
- *
- * @author ramesh
- */
+@Named("form")
+@SessionScoped
 public class RegisterForm implements Serializable {
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String country;
+    
+    private static final Locale[] countries = {Locale.US, Locale.CANADA};
+    
+    public Locale[] getCountries() {
+        return countries;
+    }
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
     
 }
