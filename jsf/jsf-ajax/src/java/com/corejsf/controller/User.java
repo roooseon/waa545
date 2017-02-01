ebe51a1a77bf2b74a8a0645444e7ca080fe7aa49
Ramesh KC <kcramesh8@gmail.com>
2017-02-01 17:26:14 -0600

    jsf-ajax project setup.

---
+++ jsf/jsf-ajax/src/java/com/corejsf/controller/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class User implements Serializable {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+}
