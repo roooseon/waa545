4a7b82c9bd0b7cd042cf2fd40028666aadf8b86f
Ramesh KC <kcramesh8@gmail.com>
2017-01-31 20:37:31 -0600

    composite component icon example.

---
+++ jsf/composite-component/src/java/com/corejsf/controller/UserBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("user")
+@SessionScoped
+public class UserBean implements Serializable {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String logout(){
+        return "index" + "?faces-redirect=true";
+    }
+}
