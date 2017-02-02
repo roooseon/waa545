b9d9aef734a97295fdefb1318835294b62f03f32
Ramesh KC <kcramesh8@gmail.com>
2017-02-01 20:31:18 -0600

    adding UsearBean class.

---
+++ jsf/ajax-validator/src/java/com/corejsf/controller/UserBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+
+@Named("user")
+@SessionScoped
+public class UserBean implements Serializable{
+    private String name = "";
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
+    public void validateName(FacesContext fc, UIComponent c, Object value) {
+        if (((String)value).contains("_")) {
+              throw new ValidatorException(new FacesMessage("Name Cannot contain underscore"));
+        }
+    }
+}
